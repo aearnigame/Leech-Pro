63dac583ee91ae7ddd30d7e4718865cf9253060b
aearnigame <106689620+aearnigame@users.noreply.github.com>
2022-06-06 21:26:53 +0530

    Update __init__.py

--- tobrot/__init__.py
+++ tobrot/__init__.py
@@ -25,7 +25,7 @@ logging.basicConfig(
     datefmt="%d-%b-%y %H:%M:%S",
     handlers=[
         RotatingFileHandler(
-            "Torrentleech-Gdrive.txt", maxBytes=50000000, backupCount=10
+            "Torrentleech-Gdrive.txt", maxBytes=5000000000, backupCount=10
         ),
         logging.StreamHandler(),
     ],
@@ -51,31 +51,31 @@ for imp in ["TG_BOT_TOKEN", "APP_ID", "API_HASH", "OWNER_ID", "AUTH_CHANNEL"]:
         exit()
 
 # The Telegram API things
-TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
-APP_ID = int(os.environ.get("APP_ID", "12345"))
-API_HASH = os.environ.get("API_HASH")
-OWNER_ID = int(os.environ.get("OWNER_ID", "539295917"))
+TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "5583961779:AAEwUaBbCASqFOTMIba9pdZe6V5JlwsPlMw")
+APP_ID = int(os.environ.get("APP_ID", "13689511"))
+API_HASH = os.environ.get("API_HASH", "c4527c8e214ef2450e352ce3a24a7454"))
+OWNER_ID = int(os.environ.get("OWNER_ID", "1301592091"))
 
 # Get these values from my.telegram.org
 # to store the channel ID who are authorized to use the bot
-AUTH_CHANNEL = [int(x) for x in os.environ.get("AUTH_CHANNEL", "539295917").split()]
+AUTH_CHANNEL = [int(x) for x in os.environ.get("AUTH_CHANNEL", "TZXhe2idA9c0NDM9").split()]
 
 # the download location, where the HTTP Server runs
 DOWNLOAD_LOCATION = "./DOWNLOADS"
 # Telegram maximum file upload size
-MAX_FILE_SIZE = 50000000
+MAX_FILE_SIZE = 5000000000
 TG_MAX_FILE_SIZE = 2097152000
-FREE_USER_MAX_FILE_SIZE = 50000000
-AUTH_CHANNEL.append(539295917)
+FREE_USER_MAX_FILE_SIZE = 500000000
+AUTH_CHANNEL.append(ddJYkxn12NdkOWI1)
 AUTH_CHANNEL.append(OWNER_ID)
 # chunk size that should be used with requests
 CHUNK_SIZE = int(os.environ.get("CHUNK_SIZE", "128"))
 # default thumbnail to be used in the videos
 DEF_THUMB_NAIL_VID_S = os.environ.get("DEF_THUMB_NAIL_VID_S", "https://telegra.ph/file/3a7f09b89943b51cdba38.jpg")
 # maximum message length in Telegram
-MAX_MESSAGE_LENGTH = 4096
+MAX_MESSAGE_LENGTH = 40960
 # set timeout for subprocess
-PROCESS_MAX_TIMEOUT = 3600
+PROCESS_MAX_TIMEOUT = 36000
 #
 SP_LIT_ALGO_RITH_M = os.environ.get("SP_LIT_ALGO_RITH_M", "hjs")
 ARIA_TWO_STARTED_PORT = int(os.environ.get("ARIA_TWO_STARTED_PORT", "6800"))
